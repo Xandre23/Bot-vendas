beef10cdac0afabca177f2d8b9b9bd6cc9c667fc
Xandre <68565199+Xandre23@users.noreply.github.com>
2025-11-04 13:57:45 -0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [
+    {
+      name: "Bot",
+      script: "index.js" // O seu bot do QR Code
+    },
+    {
+      name: "Painel",
+      script: "server.js" // O seu painel web
+    }
+  ]
+};
